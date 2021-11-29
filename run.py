65ebec08574fb645b6c6d434ec9ffa443266778a
jossansik <jossansik@gmail.com>
2021-11-29 20:31:12 +0100

    Print indices of letters occurring in word

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import random
 from words import words
 
+class User:
+    def __init__(self):
+        self.occurring_letters = []
+
 def play_game():
     random_word = random.choice(words).upper()
     word_chars = list(random_word)
@@ -12,5 +16,10 @@ def play_game():
     letter = input('To make your guess, choose a letter from A-Z & press enter: ')
 
     print(letter)
+
+     # Get all chars from actual word where we have a match.
+    valid_indices = [index for index, char in enumerate(word_chars) if letter in char]
+    
+    print(valid_indices)
 
 play_game()
