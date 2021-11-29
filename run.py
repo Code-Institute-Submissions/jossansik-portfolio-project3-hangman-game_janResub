9e84e1db0028f6b71e9654ebbf127844e6a85959
jossansik <jossansik@gmail.com>
2021-11-29 20:20:54 +0100

    Add import random and words, get random word and print it as a char array

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+import random
+from words import words
+
 def play_game():
+    random_word = random.choice(words).upper()
+    word_chars = list(random_word)
+
+    print(word_chars)
+
     print("Let's play the hangman game!")
 
     letter = input('Enter a letter A-Z')
