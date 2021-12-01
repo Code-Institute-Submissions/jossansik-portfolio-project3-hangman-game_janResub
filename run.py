fe9e145e06575c66870856c7f0641a6037b99df2
jossansik <jossansik@gmail.com>
2021-12-01 14:06:01 +0100

    Add a while loop to iterate and allow user to progress in the game

--- run.py
+++ run.py
@@ -13,33 +13,34 @@ def play_game():
 
     print("Let's play the hangman game!")
 
-    letter = input('To make your guess, choose a letter from A-Z & press enter: ')
-
-    print(letter)
-
-     # Get all chars from the word where there is a match with occuring letters
-    valid_indices = [index for index, char in enumerate(word_chars) if letter in char]
-
-    print(valid_indices)
-
     user = User()
 
-     # Validates the players input
-    for index in valid_indices:
-        # Adds the guessed letter on all occurring indices
-        if index not in user.occurring_letters:
-            user.occurring_letters.append(index)
+    while(1):
+        letter = input('To make your guess, choose a letter from A-Z & press enter: ')
 
-    print(user.occurring_letters)
+        print(letter)
 
-    total_list = ''
-    #Validate the players char
-    for index, char in enumerate(word_chars):
-        if index in user.occurring_letters:
-            total_list += word_chars[index] + ' '
-        else:
-            total_list += '_ '
+        # Get all chars from the word where there is a match with occuring letters
+        valid_indices = [index for index, char in enumerate(word_chars) if letter in char]
 
-            print(total_list)    
+        print(valid_indices)
+
+        # Validates the players input
+        for index in valid_indices:
+            # Adds the guessed letter on all occurring indices
+            if index not in user.occurring_letters:
+                user.occurring_letters.append(index)
+
+        print(user.occurring_letters)
+
+        total_list = ''
+        #Validate the players char
+        for index, char in enumerate(word_chars):
+            if index in user.occurring_letters:
+                total_list += word_chars[index] + ' '
+            else:
+                total_list += '_ '
+
+        print(total_list)    
 
 play_game()
